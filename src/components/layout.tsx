cd71c6a35e9b3b0f51955bf676bedec231e78a83
Kichan <rlcks01537@gmail.com>
2023-06-30 16:25:38 +0900

    Edit Layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -22,9 +22,9 @@ export default function Layout({
   };
   return (
     <div>
-      <div className="bg-white w-full h-12 max-w-xl justify-center text-lg px-10 font-medium  fixed text-gray-800 border-b top-0  flex items-center">
+      <div className="bg-white w-full h-12 max-w-xl justify-center text-lg px-10 font-medium  fixed text-gray-800 border-b top-0  flex items-center left-0">
         {canGoBack ? (
-          <button onClick={onClick} className="absolute left-4">
+          <button onClick={onClick} className="absolute left-0 ">
             <svg
               className="w-6 h-6"
               fill="none"
@@ -47,7 +47,7 @@ export default function Layout({
       </div>
       <div className={cls("pt-16", hasTabBar ? "pb-24" : "")}>{children}</div>
       {hasTabBar ? (
-        <nav className="bg-white max-w-xl text-gray-700 border-t fixed bottom-0 w-full px-10 pb-5 pt-3 flex justify-between text-xs">
+        <nav className="bg-white max-w-xl text-gray-700 border-t fixed bottom-0 w-full flex justify-between text-xs left-0 p-4">
         <Link href="/" className={cls(
                 "flex flex-col items-center space-y-2 ",
                 router.pathname === "/"
