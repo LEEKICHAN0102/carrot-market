6b8ab7237004206ffd0f176210d340d2473d78f3
Kichan <rlcks01537@gmail.com>
2023-07-03 16:37:38 +0900

    Twilio env

--- src/libs/server/withHandler.ts
+++ src/libs/server/withHandler.ts
@@ -1,7 +1,12 @@
 import { NextApiRequest,NextApiResponse } from "next";
 import { resourceUsage } from "process";
 
-export default function withHandler(method:"GET"|"POST"|"DELETE",fn:(req: NextApiRequest,res: NextApiResponse)=>void){
+export interface ResponseType{
+  ok:boolean;
+  [key:string]:any;
+}
+
+export default function withHandler(method:"GET"|"POST"|"DELETE",fn:(req: NextApiRequest,res: NextApiResponse<ResponseType>)=>void){
   return async function (req: NextApiRequest,res: NextApiResponse){
     if(req.method !== method){
       return res.status(405).end();
--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -1,11 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@/libs/server/client";
-import withHandler from "@/libs/server/withHandler";
+import withHandler,{ResponseType} from "@/libs/server/withHandler";
 
-
-async function handler(req: NextApiRequest,res: NextApiResponse) {
+async function handler(req: NextApiRequest,res: NextApiResponse<ResponseType>) {
   const {phone,email}=req.body;
-  const user = phone ? { phone: +phone } : { email };
+  const user = phone ? { phone: +phone } :email? { email }:null;
+  if(!user) return res.status(400).json({ok:false});
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
   const token = await client.token.create({
     data: {
@@ -24,7 +24,6 @@ async function handler(req: NextApiRequest,res: NextApiResponse) {
     },
   });
   console.log(token);
-
 //   if(email){
 //     user=await client.user.findUnique({
 //       where:{
@@ -67,7 +66,9 @@ async function handler(req: NextApiRequest,res: NextApiResponse) {
 //   }
 //   console.log(user);
 //   }
-  return res.status(200).end();
+return res.json({
+  ok: true,
+});
 }
 
 export default withHandler("POST",handler);
