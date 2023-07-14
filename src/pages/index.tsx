d0a288dec4fa67345b47637b797675d63af9282b
Kichan <rlcks01537@gmail.com>
2023-07-14 12:24:35 +0900

    product Detail

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from "next";
+import { NextPage } from "next";
 import FloatingButton from "../components/floating-button";
 import Item from "../components/item";
 import Layout from "../components/layout";
@@ -7,49 +7,55 @@ import Head from "next/head";
 import useSWR from "swr";
 import { Product } from "@prisma/client";
 
-interface ProductResponse{
-  ok:boolean;
-  products:Product[];
+interface ProductResponse {
+  ok: boolean;
+  products: Product[];
 }
 
 const Home: NextPage = () => {
-  const {user,isLoading}=useUser();
-  const {data}=useSWR<ProductResponse>("/api/products");
+  const { user, isLoading } = useUser();
+  const { data } = useSWR<ProductResponse>("/api/products");
+
   return (
     <Layout title="홈" hasTabBar>
       <Head>
         <title>Home</title>
       </Head>
       <div className="flex flex-col space-y-5 ">
-        {data?.products?.map((product) => (
-          <Item
-          id={product.id}
-          key={product.id}
-          title={product.name}
-          price={product.price}
-          comments={1}
-          hearts={1}
-        />
-      ))}
-      <FloatingButton href="/products/upload">
-          <svg
-            className="h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+        {isLoading ? (
+          <p>Loading...</p> // 로딩 상태를 표시하는 예시
+        ) : (
+          data?.products?.map((product) => (
+            <Item
+              id={product.id}
+              key={product.id}
+              title={product.name}
+              price={product.price}
+              comments={1}
+              hearts={1}
             />
-          </svg>
-        </FloatingButton>
+          ))
+        )}
       </div>
+      <FloatingButton href="/products/upload">
+        <svg
+          className="h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          />
+        </svg>
+      </FloatingButton>
     </Layout>
   );
 };
+
 export default Home;
