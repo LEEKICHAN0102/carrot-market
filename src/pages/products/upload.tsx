f29e760343954ea631dcad8b374fa8855eacbb99
Kichan <rlcks01537@gmail.com>
2023-07-14 10:36:18 +0900

    product API uploaded

--- src/pages/products/upload.tsx
+++ src/pages/products/upload.tsx
@@ -5,6 +5,9 @@ import Layout from "../../components/layout";
 import TextArea from "../../components/textarea";
 import { useForm } from "react-hook-form";
 import useMutation from "@/libs/client/useMutation";
+import { useEffect } from "react";
+import { Product } from "@prisma/client";
+import { useRouter } from "next/router";
 
 interface UploadProductForm {
   name: string;
@@ -12,13 +15,24 @@ interface UploadProductForm {
   description: string;
 }
 
+interface UploadProductMutation{
+  ok:boolean;
+  product:Product;
+}
+
 const Upload: NextPage = () => {
+  const router=useRouter();
   const { register, handleSubmit } = useForm<UploadProductForm>();
-  const [uploadProduct, { loading, data }] = useMutation("/api/products");
+  const [uploadProduct, { loading, data }] = useMutation<UploadProductMutation>("/api/products");
   const onValid = (data: UploadProductForm) => {
     if (loading) return;
     uploadProduct(data);
   };
+  useEffect(()=>{
+    if(data?.ok){
+      router.push(`/products/${data.product.id}`);
+    }
+  },[data,router])
   return (
     <Layout canGoBack title="Upload Product">
       <form className="p-4 space-y-4" onSubmit={handleSubmit(onValid)}>
@@ -42,27 +56,6 @@ const Upload: NextPage = () => {
         </label>
       </div>
       <Input
-          register={register("name", { required: true })}
-          required
-          label="Name"
-          name="name"
-          type="text"
-        />
-        <Input
-          register={register("price", { required: true })}
-          required
-          label="Price"
-          name="price"
-          type="text"
-          kind="price"
-        />
-        <TextArea
-          register={register("description", { required: true })}
-          name="description"
-          label="Description"
-          required
-        />
-        <Button text={loading ? "Loading..." : "Upload item"} />        <Input
           register={register("name", { required: true })}
           required
           label="Name"
