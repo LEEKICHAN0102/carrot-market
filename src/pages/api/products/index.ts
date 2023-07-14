b6da72f703442f6735424d16fa9b170446bdeaab
Kichan <rlcks01537@gmail.com>
2023-07-14 11:33:49 +0900

    Real Database

--- src/pages/api/products/index.ts
+++ src/pages/api/products/index.ts
@@ -7,34 +7,40 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
+  if (req.method === "GET") {
+    const products = await client.product.findMany({});
+    res.json({
+      ok: true,
+      products,
+    });
+  }
+  if (req.method === "POST") {
     const {
-      body:{name,price,description},
-      session:{user},
-  }=req;
-  const product= await client.product.create({
-    data:{
-      name,
-      price:+price,
-      description,
-      image:"xx",
-      user:{
-        connect:{
-          id:user?.id
-        }
-      }
-    }
-  })
-  const profile = await client.user.findUnique({
-    where: { id: req.session.user?.id },
-  });
-  res.json({
-    ok: true,
-    product,
-  });
+      body: { name, price, description },
+      session: { user },
+    } = req;
+    const product = await client.product.create({
+      data: {
+        name,
+        price: +price,
+        description,
+        image: "xx",
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+      },
+    });
+    res.json({
+      ok: true,
+      product,
+    });
+  }
 }
 
 export default withApiSession(
   withHandler({
-    method:"POST",
+    methods:["GET","POST"],
     handler
 }));
